315237a2e27749e1e67815d9f558610ad8150b28
Mohammad <darabi.mohammad1997@gmail.com>
2024-08-15 01:51:43 +0330

    Completed online store project

---
+++ storefront/settings/dev.py
@@ -0,0 +1,37 @@
+from .common import *
+
+DEBUG = True
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+DATABASES = {
+    'default': {
+        'ENGINE': '####',
+        'NAME': '###',
+        'HOST': '######',
+        'USER': '#######',
+        'PASSWORD': '###'
+    }
+}
+
+CELERY_BROKER_URL = 'redis://redis:6379/1'
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://redis:6379/2',
+        'TIMEOUT': 10 * 60,
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
+    }
+}
+
+EMAIL_HOST = 'smtp4dev'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = 2525
+
+DEBUG_TOOLBAR_CONFIG = {
+    'SHOW_TOOLBAR_CALLBACK': lambda request: True
+}
